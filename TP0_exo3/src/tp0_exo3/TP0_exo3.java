e6ab7b5a6faf5a7bedaa13de63b04e2f1414208f
Alizée Marti <alizee.marti@epfedu.fr>
2024-09-23 16:45:56 +0200

    avancement de l'exercice 3

--- TP0_exo3/src/tp0_exo3/TP0_exo3.java
+++ TP0_exo3/src/tp0_exo3/TP0_exo3.java
@@ -4,6 +4,8 @@
  */
 package tp0_exo3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alize
@@ -15,6 +17,25 @@ public class TP0_exo3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+//Affichage des différents opérateurs
+System.out.println("Please enter the operator:");
+System.out.println("1) add");
+System.out.println("2) substract");
+System.out.println("3) multiply");
+System.out.println("4) divide");
+System.out.println("5) modulo");
+
+//Récupération de l'opérateur voulu
+Scanner sc = new Scanner(System.in);
+int operateur =sc.nextInt();
+System.out.println("Veuillez choisir votre opérateur" +operateur);
+//Demande de la première opérande
+System.out.println("Veuillez entrer la première valeur");
+double operande1 = sc.nextDouble(); //Stockage de la première valeur
+//Demande de la deuxième opérande
+System.out.println("Veuillez saisir la dexuième valeur");
+double operande2 = sc.nextDouble();
+
+            }
     
 }
