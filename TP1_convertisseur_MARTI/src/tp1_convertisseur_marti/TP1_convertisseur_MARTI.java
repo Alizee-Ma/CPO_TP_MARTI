c5d218ccebf095dd9222d13e2427aa64e6be929b
Alizée Marti <alizee.marti@epfedu.fr>
2024-09-23 18:30:49 +0200

    avancement de l'exercice 2

--- TP1_convertisseur_MARTI/src/tp1_convertisseur_marti/TP1_convertisseur_MARTI.java
+++ TP1_convertisseur_MARTI/src/tp1_convertisseur_marti/TP1_convertisseur_MARTI.java
@@ -4,6 +4,8 @@
  */
 package tp1_convertisseur_marti;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alize
@@ -15,6 +17,32 @@ public class TP1_convertisseur_MARTI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+Scanner sc = new Scanner(System.in);
+System.out.println("veuillez entrer une premiere variable");
+Double varC =sc.nextDouble();
+System.out.print("Votre variable est "+varC);
+Double varK = 0.0;
+varK = varC + 273.15;
+System.out.print("Votre temperature en degre kelvin est "+varK);
+
+    }
+    public static double CelciusversKelvin(double tCelcius) {
+        return tCelsius + 273.15;  //Opération de conversion
+    }
+    public static double KelvinVersCelcius(double tKelvin) {
+        return tKelvin - 273.15; //Temperature en celsius
+    }     
+    public static double FarenheitVersCelcius(double tFarenheit) {
+        return (tFarenheit - 32)*5/9;
+    }
+    public static double CelciusVersFarenheit(double tCelcius) {
+        return (tCelcius *9/5)+32;
+    }
+    public static double KelvinVersFarenheit(double tKelvin) {
+        return (tKelvin - 273.15)*9/5 +32;
+    }
+    public static double FarenheitVersKelvin(double tFarenheit) {
+        return (tFarenheit -32)*5/9 + 273.15;
     }
     
 }
