bfc34dbf163b68b80ac0e65a735b17ba3c233cb7
Alizée Marti <alizee.marti@epfedu.fr>
2024-09-23 18:04:34 +0200

    fin de l'exercie 1 du TP1

--- TP1_manipNombresIn_MARTI/src/tp1_manipnombresin_marti/TP1_manipNombresIn_MARTI.java
+++ TP1_manipNombresIn_MARTI/src/tp1_manipnombresin_marti/TP1_manipNombresIn_MARTI.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresin_marti;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alize
@@ -15,6 +17,28 @@ public class TP1_manipNombresIn_MARTI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+Scanner sc = new Scanner(System.in);
+System.out.println("veuillez entrer une premiere variable");
+int var1 =sc.nextInt();
+System.out.println("Veuillez rentrer une dexuieme variable");
+int var2 = sc.nextInt();
+System.out.println("Votre premiere variable est "+var1 +" et votre deuxieme varibale est "+var2);
+int somme = 0;
+int sous = 0;
+int produit = 0;
+somme = var1 +var2;
+sous = var1 - var2;
+produit = var1*var2;
+System.out.println("La somme des deux  variables vaut "+somme);
+System.out.println("La soustraction des deux variables vaut "+sous);
+System.out.println("Le produit des deux variables vaut "+produit);
+int quotiententier = 0;
+int reste = 0;
+quotiententier = var1/var2;
+System.out.println ("Le quotien entier des deux variables est "+quotiententier);
+reste = var1%var2;
+System.out.println("Le reste de la division des deux variables est "+reste);
+
     }
     
 }
