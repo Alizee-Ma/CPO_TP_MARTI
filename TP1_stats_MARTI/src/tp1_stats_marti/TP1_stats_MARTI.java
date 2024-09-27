887b4d6910f0784649f1f15980b80b68577e5161
Alizée Marti <alizee.marti@epfedu.fr>
2024-09-27 17:14:31 +0200

    avancement de l'execice 4

--- TP1_stats_MARTI/src/tp1_stats_marti/TP1_stats_MARTI.java
+++ TP1_stats_MARTI/src/tp1_stats_marti/TP1_stats_MARTI.java
@@ -4,6 +4,9 @@
  */
 package tp1_stats_marti;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author alize
@@ -15,6 +18,32 @@ public class TP1_stats_MARTI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+ // Création du tableau de 6 entiers (chaque case représente une face du dé)
+        int[] des = new int[6]; // Initialisation à 0 par défaut
+
+        // Demande à l'utilisateur de saisir un nombre entier m
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Veuillez saisir un nombre entier m (nombre d'itérations) : ");
+        int m = scanner.nextInt();
+
+        // Générateur de nombres aléatoires
+        Random random = new Random();
+
+        // Boucle de m itérations
+        for (int i = 0; i < m; i++) {
+            // Tirer un nombre aléatoire entre 0 et 5 (représentant les faces d'un dé)
+            int resultatDe = random.nextInt(6); // 0 à 5
+            // Incrémenter la case correspondante
+            des[resultatDe]++;
+        }
+
+        // Affichage du tableau résultat
+        System.out.println("\nRésultat après " + m + " itérations :");
+        for (int i = 0; i < des.length; i++) {
+            System.out.println("Face " + (i + 1) + ": " + des[i] + " occurrence(s)");
+        }
+
+        // Fermeture du scanner
+        scanner.close();
     }
-    
 }
