deeb9f79f5618f694a6ab1e1ebccc8f9df9237ce
Alizée Marti <alizee.marti@epfedu.fr>
2024-10-16 09:08:30 +0200

    avancement du projet

--- TP2_Bieres_MARTI/src/tp2_bieres_marti/TP2_Bieres_MARTI.java
+++ TP2_Bieres_MARTI/src/tp2_bieres_marti/TP2_Bieres_MARTI.java
@@ -15,6 +15,20 @@ public class TP2_Bieres_MARTI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        // Création d'une instance de BouteilleBiere
+BouteilleBiere uneBiere = new BouteilleBiere("Cuvee des trolls",7.0 ,"Dubuisson") ;
+uneBiere.lireEtiquette();
+uneBiere.decapsuler();
+System.out.println(uneBiere) ;
+BouteilleBiere secondeBiere = new BouteilleBiere("Leffe",6.6 ,"Abbaye de Leffe");
+secondeBiere.lireEtiquette();
+secondeBiere.decapsuler();
+BouteilleBiere troisBiere = new BouteilleBiere("Heineken",5.0 , "Schiltigheim ");
+troisBiere.lireEtiquette();
+troisBiere.decapsuler();
+BouteilleBiere quatreBiere = new BouteilleBiere("Kronenbourg", 5.3 , "Obernai ");
+quatreBiere.lireEtiquette();
+quatreBiere.decapsuler();
+
+}
 }
