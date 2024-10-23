b254ff1902f4fd3ad23d50c962313645d3ba1044
Alizée Marti <alizee.marti@epfedu.fr>
2024-10-23 17:07:06 +0200

    avancement du projet

--- TP2_convertisseurObjet_MARTI/src/tp2_convertisseurobjet_marti/Convertisseur.java
+++ TP2_convertisseurObjet_MARTI/src/tp2_convertisseurobjet_marti/Convertisseur.java
@@ -9,20 +9,57 @@ package tp2_convertisseurobjet_marti;
  * @author alize
  */
 public class Convertisseur {
-float CelcuisVersKelvin;
-float KelvinVersCelcuis;
-float FarenheintVersCelcuis;
-float CelcuisversFareinheit;
-float FarenheitVersKelvin;
-float KelvinVersFarenheit;
-int nbConversions;
+    // Attribut pour compter le nombre de conversions effectuées
+    private int nbConversions;
 
-public Convertisseur () {
-nbConversions = 0 ;
-}
+    // Constructeur par défaut
+    public Convertisseur() {
+        this.nbConversions = 0;
+    }
 
-@Override
-public String toString () {
-return "nb de conversions"+ nbConversions;
+    // Méthode pour convertir Celsius en Kelvin
+    public float CelciusVersKelvin(float celcius) {
+        nbConversions++;
+        return celcius + 273.15f;
+    }
+
+    // Méthode pour convertir Kelvin en Celsius
+    public float KelvinVersCelcius(float kelvin) {
+        nbConversions++;
+        return kelvin - 273.15f;
+    }
+
+    // Méthode pour convertir Fahrenheit en Celsius
+    public float FarenheitVersCelcius(float fahrenheit) {
+        nbConversions++;
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    // Méthode pour convertir Celsius en Fahrenheit
+    public float CelciusVersFarenheit(float celcius) {
+        nbConversions++;
+        return (celcius * 9 / 5) + 32;
+    }
+
+    // Méthode pour convertir Fahrenheit en Kelvin
+    public float FarenheitVersKelvin(float fahrenheit) {
+        nbConversions++;
+        float celcius = FarenheitVersCelcius(fahrenheit);
+        return CelciusVersKelvin(celcius);
+    }
+
+    // Méthode pour convertir Kelvin en Fahrenheit
+    public float KelvinVersFarenheit(float kelvin) {
+        nbConversions++;
+        float celcius = KelvinVersCelcius(kelvin);
+        return CelciusVersFarenheit(celcius);
+    }
+
+    // Redéfinition de toString() pour fournir une représentation textuelle de l'objet
+    @Override
+    public String toString() {
+        return "Nombre total de conversions effectuées : " + nbConversions;
+    }
 }
+   
 }
