a37f4c6d7ccd5e6d149d17ba8aff2e8a3581ae9f
Alizée Marti <alizee.marti@epfedu.fr>
2024-10-23 17:15:54 +0200

    fin du projet de l'exercice 2

--- TP2_convertisseurObjet_MARTI/src/tp2_convertisseurobjet_marti/TP2_convertisseurObjet_MARTI.java
+++ TP2_convertisseurObjet_MARTI/src/tp2_convertisseurobjet_marti/TP2_convertisseurObjet_MARTI.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_marti;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alize
@@ -15,17 +17,83 @@ public class TP2_convertisseurObjet_MARTI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+      Scanner scanner = new Scanner(System.in);
         Convertisseur convertisseur = new Convertisseur();
-        
-        System.out.println("25°C en Celsuis : "+ convertisseur.CelciusVersKelvin(25)+ " Kelvin");
-        System.out.println("200K en Kelvin : "+ convertisseur.KelvinVersCelcius(200) + " Celcuis");
-         System.out.println("120°F en Farenheit : " + convertisseur.FarenheitVersCelcius(120)+ " Celcius");
-        System.out.println("0°C en Celcius : " + convertisseur.CelciusVersFarenheit(0)+ " Farenheit");
-        System.out.println("89°F en Farenheit : " + convertisseur.FarenheitVersKelvin(89)+" Celcius");
-        System.out.println("345.16K en Kelvin : " + convertisseur.KelvinVersFarenheit(345.16f)+ " Farenheit");
-        
-// Affichage du nombre total de conversions
-        System.out.println(convertisseur);
+        boolean continuer = true;
+
+        while (continuer) {
+            // Affichage du menu
+            System.out.println("\n===== Menu de Conversion de Températures =====");
+            System.out.println("1. Convertir Celsius en Kelvin");
+            System.out.println("2. Convertir Kelvin en Celsius");
+            System.out.println("3. Convertir Fahrenheit en Celsius");
+            System.out.println("4. Convertir Celsius en Fahrenheit");
+            System.out.println("5. Convertir Fahrenheit en Kelvin");
+            System.out.println("6. Convertir Kelvin en Fahrenheit");
+            System.out.println("7. Quitter");
+            System.out.print("Choisissez une option : ");
+            int choix = scanner.nextInt();
+
+            float valeur, resultat;
+
+            switch (choix) {
+                case 1:
+                    System.out.print("Entrez la température en Celsius : ");
+                    valeur = scanner.nextFloat();
+                    resultat = convertisseur.CelciusVersKelvin(valeur);
+                    System.out.println(valeur + "°C = " + resultat + "K");
+                    break;
+
+                case 2:
+                    System.out.print("Entrez la température en Kelvin : ");
+                    valeur = scanner.nextFloat();
+                    resultat = convertisseur.KelvinVersCelcius(valeur);
+                    System.out.println(valeur + "K = " + resultat + "°C");
+                    break;
+
+                case 3:
+                    System.out.print("Entrez la température en Fahrenheit : ");
+                    valeur = scanner.nextFloat();
+                    resultat = convertisseur.FarenheitVersCelcius(valeur);
+                    System.out.println(valeur + "°F = " + resultat + "°C");
+                    break;
+
+                case 4:
+                    System.out.print("Entrez la température en Celsius : ");
+                    valeur = scanner.nextFloat();
+                    resultat = convertisseur.CelciusVersFarenheit(valeur);
+                    System.out.println(valeur + "°C = " + resultat + "°F");
+                    break;
+
+                case 5:
+                    System.out.print("Entrez la température en Fahrenheit : ");
+                    valeur = scanner.nextFloat();
+                    resultat = convertisseur.FarenheitVersKelvin(valeur);
+                    System.out.println(valeur + "°F = " + resultat + "K");
+                    break;
+
+                case 6:
+                    System.out.print("Entrez la température en Kelvin : ");
+                    valeur = scanner.nextFloat();
+                    resultat = convertisseur.KelvinVersFarenheit(valeur);
+                    System.out.println(valeur + "K = " + resultat + "°F");
+                    break;
+
+                case 7:
+                    continuer = false;
+                    break;
+
+                default:
+                    System.out.println("Choix invalide, veuillez réessayer.");
+            }
+
+            // Affichage du nombre total de conversions effectuées
+            if (choix != 7) {
+                System.out.println(convertisseur);
+            }
+        }
+
+        System.out.println("Merci d'avoir utilisé le convertisseur !");
+        scanner.close();
     }
-    
 }
