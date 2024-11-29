3254ccc1002d97ac9ee5a08c51d24a9247be4e4d
Alizée Marti <alizee.marti@epfedu.fr>
2024-11-29 18:17:16 +0100

    Creation des différentes epées et  batons

--- TP3_Heroic_Fantasy_MARTI/src/tp3_heroic_fantasy_marti/TP3_Heroic_Fantasy_MARTI.java
+++ TP3_Heroic_Fantasy_MARTI/src/tp3_heroic_fantasy_marti/TP3_Heroic_Fantasy_MARTI.java
@@ -15,6 +15,18 @@ public class TP3_Heroic_Fantasy_MARTI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         // Création de dexu épées
+        Epee epee1 = new Epee("Excalibur",7, 5);
+        System.out.println(epee1); // Affiche Grâce à la methode ToString
+        Epee epee2 = new Epee("Durandal",4, 7);
+        System.out.println(epee2); // Affiche Grâce à la methode ToString
+
+        // Création de deux bâtons
+        Baton baton1 = new Baton("Chêne", 4, 5);
+        System.out.println(baton1); // Affiche grâce à la methode ToString 
+        Baton baton2 = new Baton("Charme", 5, 6);
+        System.out.println(baton2); // Affiche grâce à la methode ToString 
     }
-    
 }
+    
+
